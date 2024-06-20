09abb7f7ee532fcdd99af1368e8d1b089a745bcc
samukasoares <samuel.gsoares1@gmail.com>
2024-06-20 17:33:08 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<SidebarMenu/>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue';
+import SidebarMenu  from './components/SidebarMenu.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {SidebarMenu},
+  data() {
+    return {
+      name: 'Link',
+      age: 25
+    }
+  }, 
+
+  methods:{
+    changeName(name: string){
+      this.name =name 
+      return name
+    }
+  }
+});
+</script>
+
+<style>
+
+</style>
